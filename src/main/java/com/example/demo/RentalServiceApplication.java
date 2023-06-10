51d367da6ca9ef2265832db37771325d9ca705a3
s24260 <s24260@pjwstk.edu.pl>
2023-06-10 14:33:36 +0200

    Zjazd 7: - dodanie konfiguracji swaggera

--- src/main/java/com/example/demo/RentalServiceApplication.java
+++ src/main/java/com/example/demo/RentalServiceApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Movie Rental Service REST API",
+                version = "1.0",
+                description = "This is application for rent a movie.",
+                contact = @Contact(
+                        name = "s24260",
+                        email = "s24260@pjwstk.edu.pl"
+                )
+        )
+)
 public class RentalServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentalServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RentalServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/demo/movie/controller/RentalController.java
+++ src/main/java/com/example/demo/movie/controller/RentalController.java
@@ -2,6 +2,11 @@ package com.example.demo.movie.controller;
 
 
 import com.example.demo.movie.model.Movie;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +23,116 @@ public class RentalController {
         this.restTemplate = restTemplate;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Movie.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Gateway",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found movie.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "502",
+                    description = "Bad Gateway.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "504",
+                    description = "Gateway timeout.",
+                    content = @Content
+            )
+    })
+    @Operation(summary = "Get movie by id")
     @GetMapping("/{id}")
     public ResponseEntity<Movie> getMovie(@PathVariable(value = "id") Long id) {
         return ResponseEntity.ok(restTemplate.getForEntity(movieResourceUrl + "/" + id, Movie.class).getBody());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Movie.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Gateway",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found movie.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "502",
+                    description = "Bad Gateway.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "504",
+                    description = "Gateway timeout.",
+                    content = @Content
+            )
+    })
+    @Operation(summary = "Return movie by id")
     @PutMapping("/return_movie/{id}")
     public ResponseEntity<Movie> returnMovie(@PathVariable(value = "id") Long id) {
         HttpEntity<Boolean> request = new HttpEntity<Boolean>(true);
         return ResponseEntity.ok(restTemplate.exchange(movieResourceUrl + "/update_is_available/" + id, HttpMethod.PUT, request, Movie.class).getBody());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Movie.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Gateway",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found movie.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "502",
+                    description = "Bad Gateway.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "504",
+                    description = "Gateway timeout.",
+                    content = @Content
+            )
+    })
+    @Operation(summary = "Rent movie by id")
     @PutMapping("/rent_movie/{id}")
     public ResponseEntity<Movie> rentMovie(@PathVariable(value = "id") Long id) {
         HttpEntity<Boolean> request = new HttpEntity<Boolean>(false);
